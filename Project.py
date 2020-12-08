72075125d8db6bf7fc401ff3567710c7dc23965f
Augustindou <augustin.doultremontao@gmail.com>
2020-12-08 17:50:10 +0100

    small modifs

--- Project.py
+++ Project.py
@@ -11,7 +11,7 @@ class Project:
   """
   def read_data(self,
     X1_file : str = "X1.csv",
-    Y1_file : str = "Y1.csv",):
+    Y1_file : str = "Y1.csv") -> None:
     
-    self.X1 = pd.read_csv(X1_file)
+    self.X1 = pd.read_csv(X1_file).values
     self.Y1 = pd.read_csv(Y1_file)
