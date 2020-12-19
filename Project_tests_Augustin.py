6a6a02dda3e191497acd8d3ee5a17f323ae0c578
Gauthier de Mof <gauone@hotmail.com>
2020-12-19 18:19:02 +0100

    plot of multiple scorers in gridsearch

--- Project_tests_Augustin.py
+++ Project_tests_Augustin.py
@@ -351,6 +351,48 @@ class Project:
 
 		return gs
 
+	#following code from https://scikit-learn.org/stable/auto_examples/model_selection/plot_multi_metric_evaluation.html#sphx-glr-auto-examples-model-selection-plot-multi-metric-evaluation-py
+	#param_as_abscice is the string representing a hyper-param -> exemple: 'n_estimators'
+	def plot_grid_search_perf(scoring, gs, param_as_abscice):
+		plt.figure(figsize=(13, 13))
+		plt.title("GridSearchCV evaluating using multiple scorers simultaneously", fontsize=16)
+
+		plt.xlabel(param_as_abscice)
+		plt.ylabel("Score")
+
+		ax = plt.gca()
+		ax.set_xlim(0, 402)
+		ax.set_ylim(0.73, 1)
+
+		# Get the regular numpy array from the MaskedArray
+		X_axis = np.array(gs[param_as_abscice].data, dtype=float)
+
+		for scorer, color in zip(sorted(scoring), ['g', 'k']):
+			for sample, style in (('train', '--'), ('test', '-')):
+				sample_score_mean = gs['mean_%s_%s' % (sample, scorer)]
+				sample_score_std = gs['std_%s_%s' % (sample, scorer)]
+				ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+								sample_score_mean + sample_score_std,
+								alpha=0.1 if sample == 'test' else 0, color=color)
+				ax.plot(X_axis, sample_score_mean, style, color=color,
+						alpha=1 if sample == 'test' else 0.7,
+						label="%s (%s)" % (scorer, sample))
+
+			best_index = np.nonzero(gs['rank_test_%s' % scorer] == 1)[0][0]
+			best_score = gs['mean_test_%s' % scorer][best_index]
+
+			# Plot a dotted vertical line at the best score for that scorer marked by x
+			ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+					linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+
+			# Annotate the best score for that scorer
+			ax.annotate("%0.2f" % best_score,
+						(X_axis[best_index], best_score + 0.005))
+
+		plt.legend(loc="best")
+		plt.grid(False)
+		plt.show()
+
 
 p = Project()
 
